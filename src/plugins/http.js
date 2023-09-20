92727e9200c42a014ded3a7f8e00b28302b30bfc
anojdinuranga <anojdinuranga2001@gmail.com>
2023-09-20 10:31:48 +0530

    Server Config Added

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -5,7 +5,7 @@ import router from "@/router/index.js";
 //Authed
 export let authClient = axios.create({});
 authClient.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-authClient.defaults.baseURL = 'http://localhost:8081/api';
+authClient.defaults.baseURL = 'http://13.215.175.150:8081/api';
 authClient.interceptors.response.use(undefined, function (error) {
     console.log(error);
     if(error.response.status === 401) {
@@ -18,7 +18,7 @@ authClient.interceptors.response.use(undefined, function (error) {
 // NoneAuthed
 export let client = axios.create({});
 client.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-client.defaults.baseURL = 'http://localhost:8081/api';
+client.defaults.baseURL = 'http://13.215.175.150:8081/api';
 
 export default {
     authClient,
